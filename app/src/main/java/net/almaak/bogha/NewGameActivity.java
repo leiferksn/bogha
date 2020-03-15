2b2e1d2020cce92eb56f271f5b8bdd18f8a184b5
leiferksn <leiferksn@zoho.com>
2020-03-15 17:05:36 +0100

    #4 add new game activity

---
+++ app/src/main/java/net/almaak/bogha/NewGameActivity.java
@@ -0,0 +1,16 @@
+package net.almaak.bogha;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewGameActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+
+}
